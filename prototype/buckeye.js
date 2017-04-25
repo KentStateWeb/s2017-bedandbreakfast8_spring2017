79be20fa368da93bbb186c4e4f80449c53bfb9c8
Mikekiczek <kiczek.m@gmail.com>
2017-04-25 18:55:25 -0400

    footer
    dfsndlsnglkndslg

--- prototype/buckeye.js
+++ prototype/buckeye.js
@@ -8,3 +8,18 @@ $menulink.click(function() {
   $menu.toggleClass('active');
   return false;
 });});
+/* Requires JQuery */
+
+var modal = $('.modal');
+
+$( ".btn" ).on( "click", function() {
+  $( modal ).toggleClass('modal--show');
+});
+
+$( ".overlay" ).on( "click", function() {
+  $( modal ).toggleClass('modal--show');
+});
+
+$( ".modal__close" ).on( "click", function() {
+  $( modal ).toggleClass('modal--show');
+});
